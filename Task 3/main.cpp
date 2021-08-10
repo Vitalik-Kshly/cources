9ac34b24e43dfee599ef5c512e2d35fc3c0729dd
Vitalik-Kshly <vit.volun@ukr.net>
2021-08-10 14:27:17 +0300

    Aded task 3

---
+++ Task 3/main.cpp
@@ -0,0 +1,10 @@
+#include "Scanner.h"
+
+int main()
+{
+    Scanner scanner("files/Types.cpp");
+    // scanner.ScanFile();
+    // scanner.ChangePath("files/file.cpp");
+    scanner.ScanFile();
+    return 0;
+}
